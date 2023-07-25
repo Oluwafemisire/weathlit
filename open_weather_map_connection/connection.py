23516129adb3f3fefc7d3700a5ac972227f946c7
Oluwafemisire <oluwafemisire.ojuawo@kibo.school>
2023-07-25 23:59:23 +0100

    weathlit forecast commit

--- open_weather_map_connection/connection.py
+++ open_weather_map_connection/connection.py
@@ -14,22 +14,21 @@ class WeatherMapConnection(ExperimentalBaseConnection):
     def get(self, city):
         @st.cache_data    
         def _get(city):
-            url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={self.api_key}&units=metric'
+            url = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={self.api_key}&units=metric'
             response = self.http.request('GET', url)
             weather_data = json.loads(response.data.decode('utf-8'))
-            lon = weather_data['coord']['lon']
-            lat = weather_data['coord']['lat']
-            temp = weather_data['main']['temp']
-            pressure = weather_data['main']['pressure']
-            humidity = weather_data['main']['humidity']
-            wind_speed = weather_data['wind']['speed']
-            return {
-                'lon': lon,
-                'lat': lat,
-                'temp': temp,
-                'pressure': pressure,
-                'humidity': humidity,
-                'wind_speed': wind_speed            
-                }
+            data_list = weather_data['list']
+            length = len(data_list)
+            data = { 
+                'temp': [],
+                'pressure': [],
+                'humidity': [],
+            }
+            for i in range(0, length, +7):
+                data['temp'].append(data_list[i]['main']['temp'])
+                data['pressure'].append(data_list[i]['main']['pressure'])
+                data['humidity'].append(data_list[i]['main']['humidity'])
+            return data
+            
         return _get(city)
     
--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,6 @@ But for now where would you like to get weather data for?
 """
 conn = st.experimental_connection('weathermap', type=WeatherMapConnection)
 accessor = conn.accessor()
-def get_weather_data(city):
-    data = accessor.get(city)
-    return data
 
 city = st.text_input('City')
 
@@ -27,16 +24,56 @@ city = st.text_input('City')
 with st.spinner('Getting weather results...'):
     if city:
         try:
-            data = get_weather_data(city)
+            """
+            The weather forecast for the next 5 days is shown below."""
+            data = accessor.get(city)
             temp = data['temp']
             pressure = data['pressure']
             humidity = data['humidity']
-            wind_speed = data['wind_speed']
+            
+            col1, col2, col3, col4, col5 = st.columns(5)
+            with col1:
+                st.metric('Temperature', f'{temp[0]} °C')
+            with col2:
+                st.metric('Temperature', f'{temp[1]} °C')
+            with col3:
+                st.metric('Temperature', f'{temp[2]} °C')
+            with col4:
+                st.metric('Temperature', f'{temp[3]} °C')
+            with col5:
+                st.metric('Temperature', f'{temp[4]} °C')
 
-            st.metric('Temperature', f'{temp} °C')
-            st.metric('Pressure', f'{pressure} hPa')
-            st.metric('Humidity', f'{humidity} %')
-            st.metric('Wind Speed', f'{wind_speed} m/s')
+            st.line_chart(temp)
+            
+            col6, col7, col8, col9, col10 = st.columns(5)
+            with col6:
+                st.metric('Pressure', f'{pressure[0]} Pa')
+            with col7:
+                st.metric('Pressure', f'{pressure[1]} Pa')
+            with col8:
+                st.metric('Pressure', f'{pressure[2]} Pa')
+            with col9:
+                st.metric('Pressure', f'{pressure[3]} Pa')
+            with col10:
+                st.metric('Pressure', f'{pressure[4]} Pa')
+
+            st.line_chart(pressure)
+            
+            col11, col12, col13, col14, col15 = st.columns(5)
+            with col11:
+                st.metric('Humidity', f'{humidity[0]} %')
+            with col12:
+                st.metric('Humidity', f'{humidity[1]} %')
+            with col13:
+                st.metric('Humidity', f'{humidity[2]} %')
+            with col14:
+                st.metric('Humidity', f'{humidity[3]} %')
+            with col15:
+                st.metric('Humidity', f'{humidity[4]} %')
+
+            st.line_chart(humidity)
+
         except:
-            st.error('City not found')
-
+            st.error('Error')
+            
+        
